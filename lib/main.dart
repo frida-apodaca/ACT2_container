783e8d28b2268d2837be816143b70de17da685e7
Frida_Cera <fridaapodaca541@gmail.com>
2025-03-20 18:46:31 +0000

    envi

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Frida Sofia Apodaca Cera MAT:1136"),
+          centerTitle: true,
+          titleTextStyle:
+              const TextStyle(color: Color(0xffa3d0fb), fontSize: 15),
+          backgroundColor: const Color(0xff1a2f4e),
+        ),
+        body: Center(
+          child: Container(
+            height: 150,
+            width: 300, // Ancho del contenedor
+            padding: EdgeInsets.all(16), // Espaciado interno
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xffcedff6), // Color del borde
+                width: 5, // Grosor del borde
+              ),
+              color: Color(0xff224c6e), // Color de fondo azul
+              borderRadius: BorderRadius.circular(20), // Bordes redondeados
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5), // Sombra
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: Offset(0, 3), // Desplazamiento de la sombra
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Ajusta el tamaño de la columna
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Alineación a la izquierda
+              children: [
+                Text(
+                  'ID Empleado: 12345',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white, // Color del texto
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 8), // Espacio entre los textos
+                Text(
+                  'Nombre: Juan Pérez',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Sexo: Masculino',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
